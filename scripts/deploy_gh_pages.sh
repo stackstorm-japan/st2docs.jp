b1f0ce6a85675554b7bad9ca68c779fa2789c448
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2018-03-14 14:31:04 +0900

    Updated deploy_gh_pages script to handle errors

--- scripts/deploy_gh_pages.sh
+++ scripts/deploy_gh_pages.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
-git clone -b gh-pages https://${GH_TOKEN}@github.com/stackstorm-japan/st2docs.jp.git gh-pages
+#
+# This script is mainly used by CI to update contents of gh-pages to the latest one.
+#
+
+if [ -z "${GH_TOKEN}" -o -z "${GH_REPO}" ]; then
+  echo '(ERROR) The environment variables "GH_TOKEN" and "GH_REPO" have to be set'
+  exit 1
+fi
+
+# ${GH_TOKEN} is an access token of GitHub to update GitHub pages of the repository
+# which is specified in ${GH_REPO}.
+git clone -b gh-pages https://${GH_TOKEN}@github.com/${GH_REPO}.git gh-pages
+if [ $? -ne 0 ]; then
+  echo "(ERROR) Failed to clone the remote repository from 'https://${GH_TOKEN}@github.com/${GH_REPO}.git'"
+  exit 1
+fi
 
 cd gh-pages
 
